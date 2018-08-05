7bb1d381c60447605071cd39573a513a4c7d762d
Arvin Lee <xiaohuileee@gmail.com>
2018-08-06 01:41:40 +0800

    Redis command line application basic usage

---
+++ redis-commandline/src/main/java/top/kwseeker/commandline/StudentInfo.java
@@ -0,0 +1,49 @@
+package top.kwseeker.commandline;
+
+public class StudentInfo {
+
+    private String name;
+    private int age;
+    private String sex;
+//    private String grade;
+    private String gradeFK;
+
+    public StudentInfo(String name, int age, String sex, String mathGrade, String gradeFK) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.gradeFK = gradeFK;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getGradeFK() {
+        return gradeFK;
+    }
+
+    public void setGradeFK(String gradeFK) {
+        this.gradeFK = gradeFK;
+    }
+}
---
+++ redis-commandline/src/test/java/top/kwseeker/commandline/AdvancedGuideTest.java
@@ -0,0 +1,30 @@
+package top.kwseeker.commandline;
+
+public class AdvancedGuideTest {
+
+    //发布和订阅
+
+
+    //事务
+
+
+    //脚本
+
+
+    //服务管理
+
+
+    //数据备份与恢复
+
+
+    //客户端连接
+
+
+    //使用管道技术
+    //Redis 管道技术可以在服务端未响应时，客户端可以继续向服务端发送请求，并最终一次性读取所有服务端的响应。
+
+
+    //对Redis数据进行分区
+
+
+}
